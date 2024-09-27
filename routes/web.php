a83df60ead96e21823cb0183832a9737de02e249
Josh Harris <49429928+jharris711@users.noreply.github.com>
2024-09-27 14:26:32 -0400

    Dashboard page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
@@ -16,7 +17,7 @@ Route::redirect('/', '/dashboard');
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get(
         '/dashboard',
-        fn()  => Inertia::render('Dashboard')
+        [DashboardController::class, 'index']
     )->name('dashboard');
 
     Route::resource('project', ProjectController::class);
